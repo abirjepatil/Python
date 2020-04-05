0b3edad95821a64555b2036a9d23aff1d166f3d5
Abhishek Birjepatil <abhishek_birjepatil@hotmail.com>
2020-04-04 20:08:39 -0700

    first commit

---
+++ Professor.py
@@ -0,0 +1,15 @@
+# This class provides functionality to grade students.
+# It returns a dictionary of students and grades.
+class Professor:
+    Name = ''
+    ProfessorId = 0
+    SeminarId = 0
+
+    def GradeStudensts(self, SeminarId):
+        thisdict = {
+            "StudentA": "A+",
+            "StudentB": "B",
+            "StudentC": "C"
+        }
+        return thisdict
+
---
+++ Seminar.py
@@ -0,0 +1,8 @@
+# This class provides functionality to Conduct a seminar with a professor
+class Seminar:
+    SeminarId = 0
+    ProfessorId = 0
+
+    def ConductSeminar(self, professorId):
+        print("Conducting seminar " + str(self.SeminarId) + " With Proffesor" + str(professorId))
+
